0ca589ecd9ca0ec8a45749d1214797e29590c477
Jaeden Rotondo <jae.rotondo@hotmail.ca>
2021-09-19 16:31:47 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ public class Driver {
     public static void main(String[] args){
         Consumer Consumer1 = new Consumer(152, "Upper Canada Drive", "Toronto", "M2P2A3", 19, "male", "SINGLE", "HIGHSCHOOL", "Jaeden Rotondo");
     
-        System.out.println(Consumer1.getName());    
+        System.out.println(Consumer1.toString());    
     
     }
 }
