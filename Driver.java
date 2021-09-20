e90b45a2c6dcaf1d7eaded082e1f4bcf006fd300
Jaeden Rotondo <jae.rotondo@hotmail.ca>
2021-09-20 18:12:21 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,8 +1,9 @@
 public class Driver {
     public static void main(String[] args){
-        Consumer Consumer1 = new Consumer(152, "Upper Canada Drive", "Toronto", "M2P2A3", 19, "male", "SINGLE", "HIGHSCHOOL", "Jaeden Rotondo");
-    
-        System.out.println(Consumer1.toString());    
+        Consumer consumer1 = new Consumer(152, "Upper Canada Drive", "Toronto", "M2P2A3", 19, "male", "SINGLE", "HIGHSCHOOL", "Jaeden Rotondo");
+        Consumer consumer2 = new Consumer(152, "Wagwan", "Toronto", "M2P2A3", 19, "male", "SINGLE", "HIGHSCHOOL", "Jaeden Rotondo");
+        System.out.println(consumer1.toString());    
+        System.out.println(consumer2.equals(consumer1));
     
     }
 }
